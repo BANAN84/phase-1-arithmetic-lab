62d31a2bbcd07c1ec579704a58b144bbd38bb9bf
banan <banan.faouri2001@gmail.com>
2023-12-18 23:42:14 +0300

    done

--- index.js
+++ index.js
@@ -1,2 +1,11 @@
 
+let num1 = 60;
+let num2 = 2;
+let multiply = num1 +num2 ;
+let random= Math.floor(Math.random() * 10) + 1;
+let num3= 14
+let num4= 10
+let mod = num3%num4
+let arr =[ 1,3,4,20]
+let max =Math.max(1,20);
 // Write your code here
